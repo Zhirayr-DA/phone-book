04881f3dde04cfc88d5f9e0bf021b6fd790fe1d7
Zhirayr-DA <zhirayr.shirinyan@dataart.com>
2019-08-29 23:13:00 +0400

    First commit, react app created.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <span>Test message 1.</span>
+      <p>The paragraph.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scenes/navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Navigation</h1>
+      </div>
+    );
+  }
+}
